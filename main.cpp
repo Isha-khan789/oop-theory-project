17158a1c1d54157c33b8491302c76f4a74f1d022
Isha-khan789 <161848736+Isha-khan789@users.noreply.github.com>
2024-03-01 19:09:29 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,15 @@ using namespace std;
 class Image{
     public:
     string path;
-    public:
     struct Resolution{
         int x;
         int y;
     };
-    public:
     enum Type{
         Png,
         Jpeg,
     };
-    public:
     void get_resolution();
-    public:
     void get_type();
-    public:
     void validate_path();  
 };
