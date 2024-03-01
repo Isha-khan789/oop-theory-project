6afc676c0f54cc97fdf643af12d8b0e08525afdb
Hammad M. <hammadmajid@proton.me>
2024-03-01 21:04:15 +0500

    format code

--- main.cpp
+++ main.cpp
@@ -1,41 +1,45 @@
-
-struct Resolution{
-        int x;
-        int y;
-    };
-enum Type{
-        Png,                                           
-        Jpeg,
-    };
-class Image{
-    public:
-    string path;
-    void get_resolution();
-    void get_type();
-    void validate_path();  
+struct Resolution {
+  int x;
+  int y;
 };
-enum Filter{
-        black,
-        white,
-        magenta,
-    };
-    enum Rotation{
-        right,
-        left,
-        flip,
-    };
-    enum Crop{
-        four_by_three,
-        one_to_one,
-    };
 
-class Editor : public Image{
+enum Type {
+  Png,
+  Jpeg,
+};
+
+class Image {
 public:
-void filter(Filter f);
-void Rotation(Direction d);
-void Crop(Ratio r);
+  string path;
+  void get_resolution();
+  void get_type();
+  void validate_path();
 };
-int main(){
-        
+
+enum Filter {
+  black,
+  white,
+  magenta,
+};
+
+enum Rotation {
+  right,
+  left,
+  flip,
+};
+
+enum Crop {
+  four_by_three,
+  one_to_one,
+};
+
+class Editor : public Image {
+public:
+  void filter(Filter f);
+  void Rotation(Direction d);
+  void Crop(Ratio r);
+};
+
+int main() {
+  return 0;
 }
-        
